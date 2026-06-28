f097f8e7202498842dac51416ab2c40a5653b2db
Robinson Tamilselvan <trobinuk@gmail.com>
2026-06-28 15:42:55 -0500

    Added 2 lines and col

--- dq_engine/rules/check_null_order_date.sql
+++ dq_engine/rules/check_null_order_date.sql
@@ -1,7 +1,8 @@
 -- Rule: Check for NULL order_date values
 -- Purpose: Ensure all orders have a valid order_date
-
+-- Adding 2 line
 SELECT
-    COUNT(*) AS null_order_date_Count
+    COUNT(*) AS null_order_date_Count,
+    Count(*)/50 new_col
 FROM orders
 WHERE order_date IS NULL;
